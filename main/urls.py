32425672f2dd250f0620172d7415ada2401b6b86
sorokin <isorokin1985@gmail.com>
2023-11-21 13:40:59 +0300

    hw4

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from main.views import index, contact_us, product_info
+from main.views import index, contact_us, product_info, add_product
 
 urlpatterns = [
     path('', index),
     path('contact.html', contact_us, name='contact_us'),
     path('index.html', index, name='index'),
-    path('<int:pk>/product_info/', product_info, name="product_info")
+    path('<int:pk>/product_info/', product_info, name='product_info'),
+    path('add_product', add_product, name='add_product')
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-from catalog.models import Product
+from catalog.models import Product, Category
 from main.models import ContactData
 
 # Create your views here.
 
 
 def index(request):
-
+    number_page = 0
+    if request.method == 'POST':
+        try:
+            number_page = int(request.POST.get('number_page'))
+            print(number_page)
+        except:
+            pass
+    number_objects_page = 3
+    number_objects = len(Product.objects.all())
+    number_pages = number_objects // number_objects_page + 1
     contex = {
-        'objects_list': Product.objects.all()
+        'objects_list': Product.objects.all()[number_page*number_objects_page:(number_page+1)*number_objects_page],
+        'number_pages': list(range(number_pages))
     }
     return render(request, 'main/index.html', contex)
 
@@ -28,3 +38,20 @@ def product_info(request, pk):
     }
     print(contex)
     return render(request, 'main/product_info.html', contex)
+
+def add_product(request):
+    if request.method == 'POST':
+        try:
+            new_product = {
+                'product_name': request.POST.get('product_name'),
+                'product_description': request.POST.get('product_description'),
+                'category_id': Category.objects.filter(category_name=request.POST.get('category'))[0].pk,
+                'price': int(request.POST.get('price')),
+            }
+
+            Product.objects.create(**new_product)
+            print(new_product)
+        except:
+            print('Incorrect data')
+
+    return render(request, 'main/add_product.html')

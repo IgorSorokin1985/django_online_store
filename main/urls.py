8c84351f7e6184be4ae69d014d6c1750a6654de3
sorokin <isorokin1985@gmail.com>
2023-11-29 00:05:57 +0300

    hw5 N1

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from main.views import index, contact_us, product_info, add_product
+from main.views import ProductListView, ProductDetailView, ProductCreateView, ContactCreateView
+
 
 urlpatterns = [
-    path('', index),
-    path('contact.html', contact_us, name='contact_us'),
-    path('index.html', index, name='index'),
-    path('<int:pk>/product_info/', product_info, name='product_info'),
-    path('add_product', add_product, name='add_product')
+
+    path('', ProductListView.as_view()),
+    #path('contact.html', contact_us, name='contact_us'),
+    path('contact.html', ContactCreateView.as_view(), name='contact_us'),
+    path('product_list.html', ProductListView.as_view(), name='product_list'),
+
+    path('<int:pk>/product_info/', ProductDetailView.as_view(), name='product_info'),
+    #path('add_product', add_product, name='add_product'),
+    path('product_form.html', ProductCreateView.as_view(), name='add_product')
 ]
